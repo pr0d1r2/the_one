5097226d5dcb8cc9c12b30ccdf59337f27f7359c
Marcin Nowicki <pr0d1r2@gmail.com>
2011-02-03 15:50:00 +0100

    Add currencies to project pool.

--- rule.rb
+++ rule.rb
@@ -32,6 +32,7 @@ all = {
     'polish_remote_it_worker_survey' => 'git@github.com:pr0d1r2/polish_remote_it_worker_survey.git',
     'dotmatrix'                      => 'git@github.com:doubledrones/dotmatrix.git',
     'youtubber'                      => 'git@github.com:pr0d1r2/youtubber.git',
+    'currencies'                     => 'git@github.com:doubledrones/currencies.git',
     'the_one'                        => 'git@github.com:pr0d1r2/the_one.git'
   },
   'sources' => {
