67b0ab86ecc7fde6c8566a1a19b54652c3935d3d
Marcin Nowicki <pr0d1r2@gmail.com>
2011-04-08 16:06:40 +0200

    Some corrections on look.

--- rule.rb
+++ rule.rb
@@ -4,146 +4,146 @@ Dir.chdir ENV['HOME']
 
 all = {
   'projects' => {
-    'excerpt-fu-experteer'           => {
+    'excerpt-fu-experteer' => {
       :source => 'git@github.com:experteer/excerpt-fu.git',
     },
-    'central_plexus'                 => {
+    'central_plexus' => {
       :source => 'git@github.com:doubledrones/central_plexus.git',
     },
-    'excerpt-fu'                     => {
+    'excerpt-fu' => {
       :source => 'git@github.com:doubledrones/excerpt-fu.git',
     },
-    'doubledrones.github.com'        => {
+    'doubledrones.github.com' => {
       :source => 'git@github.com:doubledrones/doubledrones.github.com.git',
     },
-    'coworking.czest.pl'             => {
+    'coworking.czest.pl' => {
       :source => 'git@github.com:doubledrones/coworking.czest.pl.git',
     },
-    'rails'                          => {
+    'rails' => {
       :source => 'git@github.com:doubledrones/rails.git',
     },
-    'metric_fu'                      => {
+    'metric_fu' => {
       :source => 'git@github.com:doubledrones/metric_fu.git',
     },
-    'remarkable'                     => {
+    'remarkable' => {
       :source => 'git@github.com:doubledrones/remarkable.git',
     },
-    'annotate_models'                => {
+    'annotate_models' => {
       :source => 'git@github.com:doubledrones/annotate_models.git',
     },
-    'i18n_routing'                   => {
+    'i18n_routing' => {
       :source => 'git@github.com:doubledrones/i18n_routing.git',
     },
-    'most_nested_hash'               => {
+    'most_nested_hash' => {
       :source => 'git@github.com:doubledrones/most_nested_hash.git',
     },
-    'iKey_generators'                => {
+    'iKey_generators' => {
       :source => 'git@github.com:pr0d1r2/iKey_generators.git',
     },
-    'pr0d1r2.github.com'             => {
+    'pr0d1r2.github.com' => {
       :source => 'git@github.com:pr0d1r2/pr0d1r2.github.com.git',
     },
-    'active_record_maintainers'      => {
+    'active_record_maintainers' => {
       :source => 'git@github.com:pr0d1r2/active_record_maintainers.git',
     },
-    'bds'                            => {
+    'bds' => {
       :source => 'git@github.com:pr0d1r2/bds.git',
     },
-    'paperclip-time-stamped'         => {
+    'paperclip-time-stamped' => {
       :source => 'git@github.com:pr0d1r2/paperclip-time-stamped.git',
     },
-    'with_filled_field_scope'        => {
+    'with_filled_field_scope' => {
       :source => 'git@github.com:pr0d1r2/with_filled_field_scope.git',
     },
-    'geokit-gem'                     => {
+    'geokit-gem' => {
       :source => 'git@github.com:pr0d1r2/geokit-gem.git',
     },
-    'active_record_geocodable'       => {
+    'active_record_geocodable' => {
       :source => 'git@github.com:pr0d1r2/active_record_geocodable.git',
     },
-    'ipv4_address'                   => {
+    'ipv4_address' => {
       :source => 'git@github.com:pr0d1r2/ipv4_address.git',
     },
-    'jQuery-AutoComplete-Matchable'  => {
+    'jQuery-AutoComplete-Matchable' => {
       :source => 'git@github.com:pr0d1r2/jQuery-AutoComplete-Matchable.git',
     },
-    'active_record_connectionless'   => {
+    'active_record_connectionless' => {
       :source => 'git@github.com:pr0d1r2/active_record_connectionless.git',
     },
-    'site_assimilator'               => {
+    'site_assimilator' => {
       :source => 'git@github.com:pr0d1r2/site_assimilator.git',
     },
-    'ebook_assimilator'              => {
+    'ebook_assimilator' => {
       :source => 'git@github.com:pr0d1r2/ebook_assimilator.git',
     },
-    'applicage'                      => {
+    'applicage' => {
       :source => 'git@github.com:doubledrones/applicage.git',
     },
-    'applicage-assets'               => {
+    'applicage-assets' => {
       :source => 'git@github.com:doubledrones/applicage-assets.git',
     },
     'polish_remote_it_worker_survey' => {
       :source => 'git@github.com:pr0d1r2/polish_remote_it_worker_survey.git',
     },
-    'dotmatrix'                      => {
+    'dotmatrix' => {
       :source => 'git@github.com:doubledrones/dotmatrix.git',
     },
-    'youtubber'                      => {
+    'youtubber' => {
       :source => 'git@github.com:pr0d1r2/youtubber.git',
     },
-    'currencies'                     => {
+    'currencies' => {
       :source => 'git@github.com:hexorx/currencies.git',
     },
-    'countries'                      => {
+    'countries' => {
       :source => 'git@github.com:hexorx/countries.git',
     },
-    'neural_processor'               => {
+    'neural_processor' => {
       :source => 'git@github.com:pr0d1r2/neural_processor.git',
     },
-    'the_one'                        => {
+    'the_one' => {
       :source => 'git@github.com:pr0d1r2/the_one.git',
     },
-    'TVShows'                        => {
+    'TVShows' => {
       :source => 'git@github.com:pr0d1r2/TVShows.git',
     },
   },
   'sources' => {
-    'SweetFM'                        => {
+    'SweetFM' => {
       :source => 'git://github.com/sweetfm/SweetFM.git',
     },
-    'workling-experteer'             => {
+    'workling-experteer' => {
       :source => 'http://github.com/experteer/workling.git',
     },
-    'grantprix-experteer'            => {
+    'grantprix-experteer' => {
       :source => 'http://github.com/experteer/grantprix.git',
     },
-    'rails-experteer'                => {
+    'rails-experteer' => {
       :source => 'git@github.com:experteer/rails.git',
     },
     'attribute_normalizer-experteer' => {
       :source => 'http://github.com/experteer/attribute_normalizer.git',
     },
-    'awesome_nested_set-experteer'   => {
+    'awesome_nested_set-experteer' => {
       :source => 'http://github.com/experteer/awesome_nested_set.git',
     },
-    'gettext-experteer'              => {
+    'gettext-experteer' => {
       :source => 'http://github.com/experteer/gettext.git',
     },
-    'winnie'                         => {
+    'winnie' => {
       :source => 'http://github.com/Ragnarson/winnie.git',
     },
-    'eotb_rails_plugin-ragnarson'    => {
+    'eotb_rails_plugin-ragnarson' => {
       :source => 'http://github.com/Ragnarson/eotb_rails_plugin.git',
     },
-    'vim-robot'                      => {
+    'vim-robot' => {
       :source => 'https://github.com/mrmargolis/robot-vim.git',
     },
-    'compass-susy-plugin'            => {
+    'compass-susy-plugin' => {
       :source => 'git://github.com/ericam/compass-susy-plugin.git',
     },
   },
   'sources_svn' => {
-    'iTerm2'                         => {
+    'iTerm2' => {
       :source => 'http://iterm2.googlecode.com/svn/trunk/'
     },
   }
@@ -164,7 +164,7 @@ all.each do |directory, resources|
 
   resources.each do |resource_name, repository|
     puts
-    puts "#{resource_name} (#{repository})"
+    puts "#{resource_name} (#{repository[:source]})"
     if File.exist?(File.join(resource_name, '.rvmrc'))
       system("rvm rvmrc trust #{resource_name}")
     end
@@ -178,9 +178,9 @@ all.each do |directory, resources|
       Dir.chdir('..')
     else
       if svn
-        system("svn checkout #{repository} #{resource_name}")
+        system("svn checkout #{repository[:source]} #{resource_name}")
       else
-        system("git clone --recursive #{repository} #{resource_name}")
+        system("git clone --recursive #{repository[:source]} #{resource_name}")
       end
     end
   end
